ada096031e285cab15a4a7ca3b53ee67117af913
PerrinWaldock <PerrinWaldock@users.noreply.github.com>
2025-12-07 14:47:54 -0800

    update starting file

--- d0p0.py
+++ d0p0.py
@@ -2,17 +2,22 @@ import os
 import numpy as np
 from functools import cache
 
-filename = "sample.txt"
-# filename = "input.txt"
+filenames = ["sample.txt", "input.txt"]
 
 def main():
-    lines = getLines()
+    for filename in filenames:
+        lines = getLines(filename)
+        mainFunction(lines)
 
+def mainFunction(lines):
+    pass
 
-
-def getLines():
+def getLines(filename):
     with open(os.path.join(os.path.dirname(__file__), filename)) as f:
-        return [line.strip() for line in f.readlines()]
-    
+        return [line.replace("\n", "") for line in f.readlines()]
+
+def toArray(lines):
+    return np.array([list(line) for line in lines])
+
 if __name__ == "__main__":
     main()
